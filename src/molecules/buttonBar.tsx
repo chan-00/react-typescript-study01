ced173528635685b45e3e2989954753687751226
chan-00 <a82316@naver.com>
2023-04-27 17:21:35 +0900

    tiny image 적용

--- src/molecules/buttonBar.tsx
+++ src/molecules/buttonBar.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
 interface ButtonBarType {
     children: React.ReactNode,
     className?: string,
 }
 
+interface ButtonBarStyleType {
+    childrenCount: number,
+}
+
+const ButtonBarBox = styled.div<ButtonBarStyleType>`
+  display: grid;
+  grid-template-columns: repeat(${props => props.childrenCount}, 1fr);
+`;
+
 const ButtonBar = ({ children, className }: ButtonBarType) => {
 
     return (
-        <div className={className}>
+        <ButtonBarBox className={className} childrenCount={React.Children.count(children)}>
             {children}
-        </div>
+        </ButtonBarBox>
     )
 }
 
--- src/organisms/buttonImageContainer.tsx
+++ src/organisms/buttonImageContainer.tsx
@@ -1,1 +1,28 @@
 import styled from "styled-components";
+import React, {useState} from "react";
+import {ButtonBar} from "../molecules/buttonBar";
+import {Button} from "../atoms/button";
+import {Img} from "../atoms/img";
+
+const ButtonImageContainerBox = styled.div`
+  width: 100%;
+`;
+
+const ButtonBarBox = styled(ButtonBar)`
+  width: 100%;
+`;
+
+const ButtonImageContainer = () => {
+
+    const imageSrc = useState<string>();
+
+    return (
+        <ButtonImageContainerBox>
+            <ButtonBarBox>
+
+            </ButtonBarBox>
+        </ButtonImageContainerBox>
+    )
+}
+
+export {ButtonImageContainer};
